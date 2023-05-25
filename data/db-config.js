4a005c54e17a6bf9417b82d9c4b400fab74480db
Ömür <omurcanuslu@outlook.com>
2023-05-25 17:27:08 +0300

    Update db-config.js

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,5 @@
-const knex = require("knex");
-const knexConfig = require("../knexfile");
-const environment = "development";
-
-module.exports = knex(knexConfig[environment]);
+module.exports = {
+    PORT: process.env.PORT || 9000,
+    NODE_ENV: process.env.NODE_ENV || "development",
+    JWT_SECRET: process.env.JWT_SECRET || "shh",
+  };
