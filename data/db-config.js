e195fc4b2aa76228bebd30d073d7f801179112c3
Ömür <omurcanuslu@outlook.com>
2023-05-25 23:14:13 +0300

    node_env

--- data/db-config.js
+++ data/db-config.js
@@ -1,3 +1,6 @@
-module.exports = {
-    NODE_ENV: process.env.NODE_ENV || "development",
-  };
+const knexConfig = require("../knexfile");
+
+const knex = require("knex");
+const { NODE_ENV } = require("../config/index");
+
+module.exports = knex(knexConfig[NODE_ENV]);
