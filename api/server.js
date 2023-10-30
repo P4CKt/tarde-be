c3be3a4ff34e19cea5bbfee8ddb145d57bf85e63
Ömürcan Uslu <116915510+P4CKt@users.noreply.github.com>
2023-10-30 22:40:17 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const helmet = require("helmet");
 const userRouter = require("./auth/auth-router");
 const tableRouter = require("./table/table-router");
